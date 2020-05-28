b1d037eb1246c4c01836e94cb19b3c7a6b17a62a
Daddy <Daddy@vivs-macpro-13>
2020-05-28 15:57:44 -0700

    did this 5/28

---
+++ src/_02_nested_loops/_3_for_loop_gauntlet/LoopGauntlet.java
@@ -0,0 +1,32 @@
+package _02_nested_loops._3_for_loop_gauntlet;
+
+import javax.swing.JOptionPane;
+
+public class LoopGauntlet {
+	public static void main(String[] args) {
+		//for (int i = 0; i <= 100; i++) {
+		//System.out.println(i);
+		//}
+		//for (int i = 100; i >= 0; i--) {
+			//System.out.println(i);
+		//}
+		//for (int i = 2; i <= 100; i++) {
+			//System.out.println(i++);
+		//}
+		//for (int i = 1; i <= 99; i++) {
+			//System.out.println(i++);
+		//}
+		//for (int i = 0; i <= 500; i++) {
+			//if(i%2==0) {
+				//System.out.println(i + " is even");
+			//} else {
+				//System.out.println(i + " is odd");
+			//}
+		//}
+		for (int i = 0; i <= 777; i++) {
+			if(i%7==0) {
+				System.out.println(i);
+			}
+		}
+	}	
+}
