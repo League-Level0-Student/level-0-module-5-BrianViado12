8692089a03610111874a46f61010577315f9634f
Daddy <Daddy@vivs-macpro-13>
2020-06-18 15:55:44 -0700

    did this 6/18

--- src/_04_practice/_2_code_4_life/Code4Life.java
+++ src/_04_practice/_2_code_4_life/Code4Life.java
@@ -14,17 +14,24 @@ public static void main(String[] args) {
 
 	// 1. Ask the user how many hours they spent coding this week.
 	String answer = JOptionPane.showInputDialog("How many hours did you spend coding this week?");
+	int num = Integer.parseInt(answer);
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
-	if(answer%2==0) {
+	if(num<=2) {
 		JOptionPane.showMessageDialog(null, "You need to stop watching YouTube and write code instead.");
 	}
 	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
 	// they're a Code Ninja
-
+	else if(num>=3 && num<=5) {
+		JOptionPane.showMessageDialog(null, "You're a Code Ninja.");
+	}
 	// 4. If it is more than 5, call the method below to play the Batman theme song.
 	// NOTE: You may have to get batman.wav from league-sounds on github: https://github.com/jointheleague/league-sounds
+	else if (num>5) {
+		playBatmanTheme();
+	}
 }
+
 	private static void playBatmanTheme() {
 		try {
 			AudioClip sound = JApplet.newAudioClip(Code4Life.class.getResource("batman.wav"));
