236863601455276182995ef8ab1f16419106f348
Daddy <Daddy@vivs-macpro-13>
2020-07-06 11:26:17 -0700

    did this and Tests 7/6

--- src/_02_nested_loops/_3_for_loop_gauntlet/LoopGauntlet.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/LoopGauntlet.java
@@ -49,12 +49,12 @@ public class LoopGauntlet {
 			//}
 			//System.out.println();
 		//}
-		//for (int i = 1; i < 7; i++) {
-			//for (int j = 0; j < i; j++) {
-				//System.out.print("*" + " ");
+		for (int i = 1; i < 7; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("*" + " ");
 				
-			//}
-			//System.out.println();
-		//}
+			}
+			System.out.println();
+		}
 	}	
 }
